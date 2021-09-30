ec205accab2f50c2de5a220b22c1dd7a2f8ae792
mattosdu <eduardo.mattos.contact@gmail.com>
2021-09-29 22:44:57 -0300

    UP

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,7 +118,7 @@ $(function () {
 
 
     $('.circle-1').circleProgress({
-        value: 0.30,
+        value: 0.89,
         size: 145,
         thickness: 6,
         lineCap: "round",
@@ -126,7 +126,7 @@ $(function () {
             gradient: ["#ffc50c", "#ffc50c"]
         }
     }).on('circle-animation-progress', function (event, progress) {
-        $(this).find('strong').html(Math.round(30 * progress) + '<i>%</i>');
+        $(this).find('strong').html(Math.round(89 * progress) + '<i>%</i>');
     });
 
 
